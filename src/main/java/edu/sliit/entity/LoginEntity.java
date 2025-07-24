ae7e17b672889ec791f6f39b912f8968fd18e413
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2025-07-24 21:39:05 +0530

    login entity created

---
+++ src/main/java/edu/sliit/entity/LoginEntity.java
@@ -0,0 +1,22 @@
+package edu.sliit.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "login")
+public class LoginEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
